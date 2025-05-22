932ec37e8d113fdf891212ae002b0d4d9ff60a84
hetx19 <hetsavaliya19@gmail.com>
2025-05-22 10:14:48 +0530

    Structuring The Website

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
+// Components
+import About from "@/components/about";
+import Intro from "@/components/intro";
+import Skills from "@/components/skills";
+import Contact from "@/components/contact";
+import Divider from "@/components/divider";
+import Projects from "@/components/projects";
+import Experience from "@/components/experience";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <main className="flex flex-col items-center px-4">
+      <Intro />
+      <Divider />
+      <About />
+      <Projects />
+      <Skills />
+      <Experience />
+      <Contact />
+    </main>
+  );
 }
